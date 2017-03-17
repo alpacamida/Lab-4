305dbfb1bca3bf068ecb0d510ff089432af0af0f
Robert Xue <rxue@groupon.com>
2017-03-16 20:32:02 -0500

    Stage 5

---
+++ src/main/java/Rider.java
@@ -0,0 +1,26 @@
+import java.awt.*;
+
+public class Rider extends GridObject {
+    private SharedCar driver;
+    private boolean wait = true;
+
+    public Rider() {
+        this.setSymbol("R");
+        setColor(Color.YELLOW);
+        driver = null;
+    }
+
+    public boolean waiting() {
+        return wait;
+    }
+
+    public void pickUp(SharedCar c) {
+        driver = c;
+        wait = false;
+    }
+
+    public SharedCar getDriver() {
+        return driver;
+    }
+
+}
